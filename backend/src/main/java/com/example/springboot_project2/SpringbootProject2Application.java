95e8175c9473a8d946a1507a2a814515814caa6e
Bolin <jk1698oxx2@outlook.com>
2026-06-08 16:04:41 +0800

    chore: configure build settings for backend deployment

---
+++ backend/src/main/java/com/example/springboot_project2/SpringbootProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject2Application.class, args);
+	}
+
+}
